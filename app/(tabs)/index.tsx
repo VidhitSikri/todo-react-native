3d7994c868631db7ac46aac7d37b8e8f92f47e91
VidhitSikri <sikrividhit@gmail.com>
2026-01-27 12:35:46 +0530

    fixed some stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,4 @@
-import { api } from "@/convex/_generated/api";
 import { useTheme } from "@/hooks/useTheme";
-import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
