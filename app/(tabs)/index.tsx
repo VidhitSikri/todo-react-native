b31ef90e73a292ace72d569d2852cc628c70f91f
VidhitSikri <sikrividhit@gmail.com>
2026-02-04 16:29:26 +0530

    add styles for home and settings screens; update index to include api import

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { api } from "@/convex/_generated/api";
 import { useTheme } from "@/hooks/useTheme";
+import { useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
 
+  
 
 
 
