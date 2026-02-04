d0670a309fae7e3ea365edb442581d383c5c6c28
VidhitSikri <sikrividhit@gmail.com>
2026-02-04 16:50:13 +0530

    color scheme sorted out and added dark mode and light mode functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,24 @@
-import { api } from "@/convex/_generated/api";
-import { useTheme } from "@/hooks/useTheme";
-import { useQuery } from "convex/react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import {  useTheme } from "@/hooks/useTheme";
+import {  StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
 
-  
-
-
-
+  const homeStyles = createHomeStyles(colors);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.content}>
-        {" "}
-        Edit app/index.tsx to edit this screen.{" "}
-      </Text>
-      <Text>hi</Text>
-      <TouchableOpacity onPress={ toggleDarkMode }>
-        <Text>Toggle dark mode</Text>
-      </TouchableOpacity>
-
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Text>hi</Text>
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle dark mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 10,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  content: {
-    fontSize: 10,
-  },
-});
